563168d9017e8fb6d0ca7f53443a69e7f62eb564
indracb-dev19 <indra.cb.dev19@gmail.com>
2025-12-19 22:56:59 +0700

    Add api routes for get Event details

---
+++ app/api/event/[slug]/route.ts
@@ -0,0 +1,17 @@
+import { Event } from "@/database";
+import { connectToDatabase } from "@/lib/mongodb";
+import { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest, { params }: { params: { slug: string } }) {
+    try {
+        await connectToDatabase();
+        const event = await Event.findOne({ slug: params.slug }).populate('bookings');
+        if (!event) {
+            return new Response(JSON.stringify({ error: "Event not found" }), { status: 404 });
+        }
+        return new Response(JSON.stringify(event), { status: 200 });
+    } catch (error) {
+        console.error("Error fetching event:", error);
+        return new Response(JSON.stringify({ error: "Failed to fetch event" }), { status: 500 });
+    }
+}
